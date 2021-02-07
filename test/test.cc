3f43c5825775f8ee6107e1170f896b6c5e0a5401
Marcin Ryżewski <marcin.ryzewskii@gmail.com>
2021-02-07 18:07:33 +0100

    Test process memory class

--- test/test.cc
+++ test/test.cc
@@ -4,9 +4,18 @@
 int main()
 {
   
-  ProcessMemory::Process prc(6320, L"a.exe");
+  ProcessMemory::Process prc(5332, L"a.exe");
   prc.showDebudInfo();
   
+  puts("");
+
+  std::vector<DWORD> v = {0x20, 0x10, 0x05};
+
+  DWORD offstar = 0x5000;
+
+  ProcessMemory::ReadWrite<int> a(prc, offstar, v);
+  
+  a.showDebugInfo();
   
 
   return 0;
